3e25f12d766ae8758d9e1490c9806c123f87efd9
Kyle Carter <kcarter@galois.com>
2014-04-02 11:37:06 -0700

    Added Copointed

--- src/Data/LnFunctor.hs
+++ src/Data/LnFunctor.hs
@@ -13,14 +13,14 @@ module Data.LnFunctor where
 import Data.IxFunctor         (IxFunctor (..))
 import Data.IxFunctor.Apply   (IxApply   (..))
 import Data.IxFunctor.Empty   (IxEmpty   (..))
-import Data.IxFunctor.Alt
+import Data.IxFunctor.Alt     (IxAlt     (..))
 import Data.IxFunctor.Pointed (IxPointed (..))
 import Data.IxFunctor.Bind    (IxBind    (..))
-import qualified Data.IxFunctor.Bind as Bind
+import Data.IxFunctor.Copointed (IxCopointed (..))
 
 import GHC.Exts (Constraint)
 
-type FunctorLink  f i j = (LnFunctor f, Link f i j)
+type FunctorLink f i j = (LnFunctor f, Link f i j)
 type Rfl
   (c :: (ix -> ix -> * -> *) -> ix -> ix -> k)
   (f ::  ix -> ix -> * -> *)
@@ -33,11 +33,11 @@ class LnFunctor f => LnTerminal (f :: ix -> ix -> * -> *) where
   type Term f :: ix -> ix -> Constraint
 
 class LnFunctor f => LnJoin (f :: ix -> ix -> * -> *) where
-  type JoinEn f :: ix -> ix -> Constraint
-  type JoinEx f :: ix -> ix -> Constraint
+  type GatherUp   f :: ix -> ix -> Constraint
+  type GatherDown f :: ix -> ix -> Constraint
 class LnFunctor f => LnSplit (f :: ix -> ix -> * -> *) where
-  type SplitEn f :: ix -> ix -> Constraint
-  type SplitEx f :: ix -> ix -> Constraint
+  type UpSplit   f :: ix -> ix -> Constraint
+  type DownSplit f :: ix -> ix -> Constraint
 
 -- Functor {{{
 
@@ -52,12 +52,12 @@ class LnFunctor (f :: ix -> ix -> * -> *) where
   lmap = lmapWeaken
   -- map with weaken
   lmapWeaken :: Link f i j => (a -> b) -> f j k a -> f i k b
-  default lmapWeaken :: (IxFunctor f, FunctorLink f i j)
+  default lmapWeaken :: (IxFunctor f, Link f i j)
     => (a -> b) -> f j k a -> f i k b
   lmapWeaken f = weaken . imap f
   -- map with strengthen
   lmapStrengthen :: Link f j k => (a -> b) -> f i j a -> f i k b
-  default lmapStrengthen :: (IxFunctor f, FunctorLink f j k)
+  default lmapStrengthen :: (IxFunctor f, Link f j k)
     => (a -> b) -> f i j a -> f i k b
   lmapStrengthen f = strengthen . imap f
 
@@ -81,7 +81,7 @@ lvoid = (() <$$)
 
 class (LnFunctor f, LnInitial f) => LnEmpty f where
   lempty :: Init f i j => f i j a
-  default lempty :: (IxEmpty f, FunctorLink f i j)
+  default lempty :: (IxEmpty f, Link f i j, Init f i j)
     => f i j a
   lempty = weaken $ iempty
 
@@ -94,7 +94,7 @@ rempty = lempty
 
 class (LnFunctor m, LnInitial m) => LnPointed (m :: ix -> ix -> * -> *) where
   lreturn :: Init m i j => a -> m i j a
-  default lreturn :: (IxPointed m, FunctorLink m i j)
+  default lreturn :: (IxPointed m, Link m i j, Init m i j)
     => a -> m i j a
   lreturn = weaken . ireturn
 
@@ -108,7 +108,7 @@ rreturn = lreturn
 
 class LnFunctor m => LnApply m where
   lap :: Link m j k => m i j (a -> b) -> m k l a -> m i l b
-  default lap :: (IxApply m, FunctorLink m j k)
+  default lap :: (IxApply m, Link m j k)
     => m i j (a -> b) -> m k l a -> m i l b
   lap f = iap f . weaken
 
@@ -153,20 +153,35 @@ infixl 4 **>
 -- Alt {{{
 
 class (LnFunctor m, LnJoin m) => LnAlt m where
-  laltL    :: JoinEn m i j => m i k a -> m j k a -> m i k a
-  laltR    :: JoinEn m i j => m i k a -> m j k a -> m j k a
-  lfunnelL :: JoinEx m j k => m i j a -> m i k a -> m i j a
-  lfunnelR :: JoinEx m j k => m i j a -> m i k a -> m i k a
+  lbranchL :: GatherUp m i j => m i k a -> m j k a -> m i k a
+  default lbranchL :: (IxAlt m, Link m i j, GatherUp m i j) => m i k a -> m j k a -> m i k a
+  lbranchL m1 m2 = ialt m1 (weaken m2)
+
+  lbranchR :: GatherUp m i j => m i k a -> m j k a -> m j k a
+  default lbranchR :: (IxAlt m, Link m j i, GatherUp m i j) => m i k a -> m j k a -> m j k a
+  lbranchR m1 m2 = ialt (weaken m1) m2
+
+  lfunnelL :: GatherDown m j k => m i j a -> m i k a -> m i j a
+  default lfunnelL :: (IxAlt m, Link m k j, GatherDown m j k) => m i j a -> m i k a -> m i j a
+  lfunnelL m1 m2 = ialt m1 (strengthen m2)
+
+  lfunnelR :: GatherDown m j k => m i j a -> m i k a -> m i k a
+  default lfunnelR :: (IxAlt m, Link m j k, GatherDown m j k) => m i j a -> m i k a -> m i k a
+  lfunnelR m1 m2 = ialt (strengthen m1) m2
+
+(<||>) :: (LnAlt m, Rfl GatherUp m i) => m i j a -> m i j a -> m i j a
+(<||>) = lbranchL
 
 -- }}}
 
-{-
 -- Bind {{{
 
 class LnApply m => LnBind m where
   lbind :: Link m j k => m i j a -> (a -> m k l b) -> m i l b
+  default lbind :: (IxBind m, Link m j k) => m i j a -> (a -> m k l b) -> m i l b
+  lbind m f = ibind m $ weaken . f
 
-rbind :: (LnBind m, Reflex Link m j) => m i j a -> (a -> m j k b) -> m i k b
+rbind :: (LnBind m, Rfl Link m j) => m i j a -> (a -> m j k b) -> m i k b
 rbind = lbind
 
 ljoin :: (LnBind m, Link m j k) => m i j (m k l a) -> m i l a
@@ -194,39 +209,52 @@ infixr 1 <=<<
 
 -- }}}
 
+-- Monad {{{
+
 type LnMonad  m = (LnPointed m, LnBind m)
 
-strengthenM :: (LnMonad m, Link m j k, Reflex Init m k)
+strengthenM :: (LnMonad m, Link m j k, Rfl Init m k)
   => m i j b -> m i k b
 strengthenM = (>>>= rreturn)
 
-lapLnMonad :: (LnMonad m, Link m j k, Reflex Link m l, Reflex Init m l)
+lapLnMonad :: (LnMonad m, Link m j k, Rfl Link m l, Rfl Init m l)
   => m i j (a -> b) -> m k l a -> m i l b
 lapLnMonad f x =
   f >>>= \f' ->
   x >>>= \x' ->
   rreturn $ f' x'
 
-lliftM :: LnMonad m => (a -> b) -> m i j a -> m i j b
-lliftM = lliftA
+rliftM :: (LnMonad m, Rfl Link m i) => (a -> b) -> m i j a -> m i j b
+rliftM = rliftA
 
-iliftM2 :: LnMonad m => (a -> b -> c) -> m i j a -> m j k b -> m i k c
-iliftM2 = iliftA2
+rliftM2 :: (LnMonad m, Rfl Link m i, Rfl Link m j) => (a -> b -> c) -> m i j a -> m j k b -> m i k c
+rliftM2 = rliftA2
 
-iliftM3 :: LnMonad m => (a -> b -> c -> d)
--> m i j a -> m j k b -> m k l c -> m i l d
-iliftM3 = iliftA3
+rliftM3 :: (LnMonad m, Rfl Link m i, Rfl Link m j, Rfl Link m k) => (a -> b -> c -> d)
+  -> m i j a -> m j k b -> m k l c -> m i l d
+rliftM3 = rliftA3
 
-isequence :: LnMonad m => [m i i a] -> m i i [a]
-isequence = foldr (iliftM2 (:)) $ ireturn []
+rsequence :: (LnMonad m, Rfl Link m i, Rfl Init m i) => [m i i a] -> m i i [a]
+rsequence = foldr (rliftM2 (:)) $ rreturn []
 
-imapM :: LnMonad m => (a -> m i i b) -> [a] -> m i i [b]
-imapM f = isequence . map f
+rmapM :: (LnMonad m, Rfl Link m i, Rfl Init m i) => (a -> m i i b) -> [a] -> m i i [b]
+rmapM f = rsequence . map f
 
-isequence_ :: LnMonad m => [m i i a] -> m i i ()
-isequence_ = foldr (>>>) $ ireturn ()
+rsequence_ :: (LnMonad m, Rfl Link m i, Rfl Init m i) => [m i i a] -> m i i ()
+rsequence_ = foldr (>>>) $ rreturn ()
 
-imapM_ :: LnMonad m => (a -> m i i b) -> [a] -> m i i ()
-imapM_ f = isequence_ . map f
--}
+rmapM_ :: (LnMonad m, Rfl Link m i, Rfl Init m i) => (a -> m i i b) -> [a] -> m i i ()
+rmapM_ f = rsequence_ . map f
+
+-- }}}
+
+-- Copointed {{{
+
+class (LnFunctor w, LnTerminal w) => LnCopointed (w :: ix -> ix -> * -> *) where
+  lextract :: Term w i j => w i j a -> a
+  default lextract :: (IxCopointed w, Link w j i, Term w i j)
+    => w i j a -> a
+  lextract = iextract . weaken
+
+-- }}}
 
